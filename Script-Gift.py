cec416be271c011188773dd1967d57365904502a
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-18 01:32:19 +0600

    Add files via upload

---
+++ Script-Gift.py
@@ -0,0 +1,9 @@
+import os,time
+print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+time.sleep(5)
+print('\033[1;34m\n JOIN MY GIFT GROUP Tools\033[1;37m')
+time.sleep(3)
+os.system('xdg-open https://www.facebook.com/Tutul.King.Ok.Bro')
+import ok
+ok.main()
+ 
