3c84d764075e713c2faf0ced2073a50298daebd8
Pro-Max-420 <121747358+Pro-Max-420@users.noreply.github.com>
2023-01-18 01:42:25 +0600

    Script-Gift.py

--- Script-Gift.py
+++ Script-Gift.py
@@ -1,9 +1,22 @@
 import os,time
-print('\033[1;34m\n Congratulations! Your Device Support This Tools\033[1;37m')
+
+print('\033[0;92m\n Congratulations! Your Device Support This Tools\033[1;37m')
+
 time.sleep(5)
-print('\033[1;34m\n JOIN MY GIFT GROUP Tools\033[1;37m')
+
+print('\033[0;93m\n JOIN MY GIFT GROUP\033[1;37m')
+
 time.sleep(3)
-os.system('xdg-open https://www.facebook.com/Tutul.King.Ok.Bro')
+
 import ok
+
 ok.main()
+
  
+
+
+
+
+
+
+
